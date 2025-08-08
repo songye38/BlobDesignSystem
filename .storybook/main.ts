df4b107768ce10067d89003a04017c225e33cf81
songye38 <ddev0308@gmail.com>
2025-08-08 18:04:48 +0900

    UPDATE 설정 변경

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,28 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest"
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      plugins: [svgrPlugin()],
+    });
+  },
 };
+
 export default config;
